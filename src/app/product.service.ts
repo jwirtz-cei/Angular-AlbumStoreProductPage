1579c8b45eeee1fc77a283262429b2abb73d96ef
jwirtz-cei <jwirtz@ceiamerica.com>
2020-06-09 11:41:34 -0400

    BuildAlbum5

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,12 +3,18 @@ import { Http, Response } from '@angular/http';
 import { Album } from './album';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
+import { map } from 'rxjs/operator/map';
 
 @Injectable()
 export class ProductService {
   constructor(private _http: Http) {}
   private _albumUrl: string = '../assets/album.json';
   getAlbum(id: number): Observable<Album>{
-    return this._http.get(this._albumUrl).map(response => <Album> response.json());
-    }
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
+  }
+   private _productsUrl: string = '../assets/products.json';
+   getProducts(): Observable<Product[]>{
+     return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
+   }
 }
